4da65efc7380bd78f7b9d99395a83ab49d314e58
tinat2 <tina.t2aq@gmail.com>
2019-07-23 17:30:20 +0430

    Add toolbar and font,#18

--- app/src/main/java/de/netalic/peacock/ui/base/BaseActivity.kt
+++ app/src/main/java/de/netalic/peacock/ui/base/BaseActivity.kt
@@ -1,6 +1,14 @@
 package de.netalic.peacock.ui.base
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-open class BaseActivity:AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initUiComponents()
+    }
+
+    abstract fun initUiComponents()
 }
--- app/src/main/java/de/netalic/peacock/ui/main/MainActivity.kt
+++ app/src/main/java/de/netalic/peacock/ui/main/MainActivity.kt
@@ -4,13 +4,34 @@ import android.os.Bundle
 import de.netalic.peacock.R
 import de.netalic.peacock.ui.base.BaseActivity
 import de.netalic.peacock.ui.registration.RegistrationFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : BaseActivity() {
+
+    private val mToolbar by lazy { toolbar_mainHost }
+    private val mTextViewToolbarTitle by lazy { textView_mainHost_toolbarTitle }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().add(R.id.frameLayout_main_container,RegistrationFragment()).commit()
+        supportFragmentManager.beginTransaction().add(R.id.frameLayout_main_container, RegistrationFragment()).commit()
+    }
+
+    override fun initUiComponents() {
+        setSupportActionBar(mToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    fun updateToolbarTitle(title: String) {
+        mTextViewToolbarTitle.text = title
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
